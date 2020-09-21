af9e39a81b30553cf810ca66c68426589d7baec5
faizp <faizkizhisseri@gmail.com>
2020-09-21 22:03:48 +0530

    added user registration

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from user import views as view_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('register/', view_user.register)
 ]
