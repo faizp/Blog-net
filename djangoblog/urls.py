af4fdce14204d7114045cf55280dc9bd7535d145
faizp <faizkizhisseri@gmail.com>
2020-09-22 10:42:36 +0530

    login and logout for users

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from user import views as view_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('', include('blog.urls')),
-    path('register/', view_user.register)
+    path('register/', view_user.register, name='register')
 ]
