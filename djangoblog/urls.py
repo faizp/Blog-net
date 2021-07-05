5c4ea2b4d039c84e1c1cd70cf1db84d75794aad9
faizp <faizkizhisseri@gmail.com>
2021-07-05 11:01:55 +0530

    setup rest

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from user import views as view_user
-
+    
 
 urlpatterns = [
     path('admin/', admin.site.urls),
